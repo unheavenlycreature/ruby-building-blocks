af0c410848c59cad01d15c1e9fa742e0f1353793
unheavenlycreature <imaginaryenemy@protonmail.com>
2020-01-26 16:52:36 -0500

    Format caesar cipher.

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,23 +1,21 @@
+# frozen_string_literal: true
+
 def shifted_character(codepoint, range_start, range_end, shift)
   codepoint += shift
-  if codepoint > range_end
-    codepoint = range_start + (codepoint % range_end) - 1
-  end
+  codepoint = range_start + (codepoint % range_end) - 1 if codepoint > range_end
   codepoint.chr
 end
 
 def caesar_cipher(to_obfuscate, shift)
-  obfuscated = ""
+  obfuscated = ''
   to_obfuscate.codepoints.each do |codepoint|
-    if (65..90) === codepoint
-      obfuscated << shifted_character(codepoint, 65, 90, shift)
-    elsif (97..122) === codepoint
-      obfuscated << shifted_character(codepoint, 97, 122, shift)
-    else
-      obfuscated << codepoint.chr
-    end
+    obfuscated << if (65..90) === codepoint
+                    shifted_character(codepoint, 65, 90, shift)
+                  elsif (97..122) === codepoint
+                    shifted_character(codepoint, 97, 122, shift)
+                  else
+                    codepoint.chr
+                  end
   end
   obfuscated
 end
-
-puts caesar_cipher("What a string!", 5)
